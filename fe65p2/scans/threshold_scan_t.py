87a956b47c66664500062d41bdcc35b553d611f3
cgottard <carlo.gottardo91@gmail.com>
2016-06-16 11:40:20 +0200

    merging done, sequential pixel inj.  not working yet

--- fe65p2/scans/threshold_scan_t.py
+++ fe65p2/scans/threshold_scan_t.py
@@ -15,11 +15,12 @@ logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(name)s - [%(leve
 local_configuration = {
     "mask_steps": 1,
     "repeat_command": 101,
-    "scan_range": [0.05, 1.5, 0.05],
-    "vthin1Dac": 60,
+    "scan_range": [0.1, 0.2, 0.1],
+    "vthin1Dac": 55,
     "preCompVbnDac" : 115,
     "columns" : [True] * 2 + [True] * 14,
-    "mask_filename": ''
+    "mask_filename": '',
+    "pix_list": [250]
 }
 
 
@@ -27,7 +28,7 @@ local_configuration = {
 class ThresholdScan(ScanBase):
     scan_id = "threshold_scan"
 
-    def scan(self, mask_steps=4, repeat_command=100, columns = [True] * 16, scan_range = [0, 1.2, 0.1], vthin1Dac = 80, preCompVbnDac = 50, mask_filename='', **kwargs):
+    def scan(self, pix_list=(200), mask_steps=4, repeat_command=101, columns = [True] * 16, scan_range = (0, 1.2, 0.1), vthin1Dac = 80, preCompVbnDac = 50, mask_filename='', **kwargs):
         '''Scan loop
         Parameters
         ----------
@@ -38,9 +39,13 @@ class ThresholdScan(ScanBase):
         '''
 
         INJ_LO = 0.0
-        dut = Dut('/home/carlo/basil/examples/lab_devices/agilent33250a_pyserial.yaml')
-        dut.init()
-        logging.info('Connected to '+str(dut['Pulser'].get_info()))
+        try:
+            dut = Dut('/home/carlo/basil/examples/lab_devices/agilent33250a_pyserial.yaml')
+            dut.init()
+            logging.info('Connected to '+str(dut['Pulser'].get_info()))
+        except RuntimeError:
+            logging.info('External injector not connected. Switch to internal one')
+            self.dut['INJ_LO'].set_voltage(INJ_LO, unit='V')
 
         self.dut['global_conf']['PrmpVbpDac'] = 80
         self.dut['global_conf']['vthin1Dac'] = 255
@@ -104,7 +109,7 @@ class ThresholdScan(ScanBase):
         self.dut['global_conf']['OneSr'] = 1
         self.dut.write_global()
 
-        self.dut['inj'].set_delay(100000)
+        self.dut['inj'].set_delay(1000000)
         self.dut['inj'].set_width(1000)
         self.dut['inj'].set_repeat(repeat_command)
         self.dut['inj'].set_en(False)
@@ -113,12 +118,16 @@ class ThresholdScan(ScanBase):
         self.dut['trigger'].set_width(16)
         self.dut['trigger'].set_repeat(1)
         self.dut['trigger'].set_en(False)
-        pixel = [128]
+
+#        pixel = [500]
         lmask = [0]*(64*64)
-        lmask[pixel[0]] = 1
+        lmask[pix_list[0]] = 1
+
 
         scan_range = np.arange(scan_range[0], scan_range[1], scan_range[2])
-        self.pixel_list = np.repeat(pixel, scan_range.shape[0])
+        self.steps = len(scan_range)
+        self.pixel_list = pix_list
+
         logging.debug('Enable TDC')
         self.dut['tdc']['RESET'] = True
         self.dut['tdc']['EN_TRIGGER_DIST'] = True
@@ -128,8 +137,10 @@ class ThresholdScan(ScanBase):
         self.dut['tdc']['EN_INVERT_TDC'] = False
         self.dut['tdc']['EN_WRITE_TIMESTAMP'] = True
         self.inj_charge = []
+
         for idx, k in enumerate(scan_range):
             dut['Pulser'].set_voltage(INJ_LO, float(INJ_LO + k), unit='V')
+            self.dut['INJ_HI'].set_voltage( float(INJ_LO + k), unit='V')
             self.inj_charge.append(float(k)*1000.0*ScanBase.cap_fac(self))
 
             time.sleep(0.5)
@@ -150,7 +161,6 @@ class ThresholdScan(ScanBase):
                 self.dut['pixel_conf'][:]  = bv_mask
                 self.dut.write_pixel()
                 self.dut['global_conf']['InjEnLd'] = 1
-                #self.dut['global_conf']['PixConfLd'] = 0b11
                 self.dut.write_global()
 
                 #bv_mask[1:] = bv_mask[0:-1]
@@ -178,8 +188,69 @@ class ThresholdScan(ScanBase):
         scan_results = self.h5_file.create_group("/", 'scan_masks', 'Scan Masks')
         self.h5_file.createCArray(scan_results, 'tdac_mask', obj=mask_tdac)
         self.h5_file.createCArray(scan_results, 'en_mask', obj=mask_en)
-        
-        
+
+
+    def tdc_table(self):
+        h5_filename = self.output_filename +'.h5'
+        with tb.open_file(h5_filename, 'r+') as in_file_h5:
+            raw_data = in_file_h5.root.raw_data[:]
+            meta_data = in_file_h5.root.meta_data[:]
+            hit_data = self.dut.interpret_raw_data(raw_data, meta_data)
+            print hit_data['col'], hit_data['row']
+            if (meta_data.shape[0]==0): return
+            param, index = np.unique(meta_data['scan_param_id'], return_index=True)
+            index = index[1:]
+            index = np.append(index, meta_data.shape[0])
+            index = index - 1
+            stops = meta_data['index_stop'][index]
+            split = np.split(raw_data, stops)
+            avg_tdc = []
+            avg_tdc_err = []
+            avg_del = []
+            avg_del_err = []
+            for i in range(len(split[:-1])): #loop on pulses
+                rwa_data_param  = split[i]
+                tdc_data = rwa_data_param & 0xFFF  # take last 12 bit
+                tdc_delay = (rwa_data_param & 0x0FF00000) >> 20
+                counter = 0.0
+                TOT_sum = 0.0
+                DEL_sum = 0.0
+                if (tdc_data.shape[0]==0): counter = 1.0
+                for j in range(tdc_data.shape[0]): #loop on repeats
+                    if (j>0):
+                       # print  j, hex(raw_data[j]), tdc_data[j], tdc_delay[j]
+                        if(j%2==0): continue
+                        counter += 1
+                        TOT_sum += tdc_data[j]
+                        DEL_sum += tdc_delay[j]
+                avg_tdc.append((float(TOT_sum)/float(counter))*1.5625)
+                avg_tdc_err.append(1.5625/(np.sqrt(12.0*counter)))
+                avg_del.append((float(DEL_sum)/float(counter))*1.5625)
+                avg_del_err.append(1.5625/(np.sqrt(12.0*counter)))
+
+
+            '''
+            If the collected injections are less than the actual ones it's because I injected below threshold.
+            In case the lenght of arrays does not match I discard the proper amount of inj_charge entries.
+            '''
+            if (len(self.inj_charge)-len(avg_tdc) > 0):
+                n = len(self.inj_charge)-len(avg_tdc)
+                self.inj_charge = self.inj_charge[n:]
+                self.pixel_list = self.pixel_list[n:]
+
+            avg_tab = np.rec.fromarrays([self.inj_charge, self.pixel_list, avg_tdc, avg_tdc_err, avg_del, avg_del_err],
+                                        dtype =[('charge', float), ('pixel_no', int), ('tot_ns', float),('err_tot_ns', float), ('delay_ns', float), ('err_delay_ns', float)])
+            in_file_h5.createTable(in_file_h5.root, 'tdc_data', avg_tab, filters=self.filter_tables)
+#plottign part
+        p1, p2 = plotting.plot_timewalk(h5_filename)
+        output_file(self.output_filename + '.html', title=self.run_name)
+        save(vplot(p1,p2))
+
+
+
+
+
+
     def analyze(self):
         h5_filename = self.output_filename +'.h5'
         with tb.open_file(h5_filename, 'r+') as in_file_h5:
@@ -200,63 +271,7 @@ class ThresholdScan(ScanBase):
         output_file(self.output_filename + '.html', title=self.run_name)
         save(vplot(hplot(occ_plot, tot_plot, lv1id_plot), scan_pix_hist, t_dac, status_plot))
 
-
-
-    def tdc_table(self):
-        h5_filename = self.output_filename +'.h5'
-        with tb.open_file(h5_filename, 'r+') as in_file_h5:
-            raw_data = in_file_h5.root.raw_data[:]
-            meta_data = in_file_h5.root.meta_data[:]
-            if (meta_data.shape[0]==0): return
-            param, index = np.unique(meta_data['scan_param_id'], return_index=True)
-            index = index[1:]
-            index = np.append(index, meta_data.shape[0])
-            index = index - 1
-            stops = meta_data['index_stop'][index]
-            split = np.split(raw_data, stops)
-            avg_tdc = []
-            avg_tdc_err = []
-            avg_del = []
-            avg_del_err = []
-            for i in range(len(split[:-1])):
-                rwa_data_param  = split[i]
-                tdc_data = rwa_data_param & 0xFFF  # take last 12 bit
-                tdc_delay = (rwa_data_param & 0x0FF00000) >> 20
-                counter = 0.0
-                TOT_sum = 0.0
-                DEL_sum = 0.0
-                if (tdc_data.shape[0]==0): counter = 1.0
-                for j in range(tdc_data.shape[0]):
-                    if (j>0):
-                       # print  j, hex(raw_data[j]), tdc_data[j], tdc_delay[j]
-                        counter += 1
-                        TOT_sum += tdc_data[j]
-                        DEL_sum += tdc_delay[j]
-                avg_tdc.append((float(TOT_sum)/float(counter))*1.5625)
-                avg_tdc_err.append(1.5625/(np.sqrt(12.0*counter)))
-                avg_del.append((float(DEL_sum)/float(counter))*1.5625)
-                avg_del_err.append(1.5625/(np.sqrt(12.0*counter)))
-            '''
-            If the collected injections are less than the actual ones it's because I injected below threshold.
-            In case the lenght of arrays does not match I discard the proper amount of inj_charge entries.
-            '''
-            if (len(self.inj_charge)-len(avg_tdc) > 0):
-                n = len(self.inj_charge)-len(avg_tdc)
-                self.inj_charge = self.inj_charge[n:]
-                self.pixel_list = self.pixel_list[n:]
-
-            avg_tab = np.rec.fromarrays([self.inj_charge, self.pixel_list, avg_tdc, avg_tdc_err, avg_del, avg_del_err],
-                                        dtype =[('charge', float), ('pixel_no', int), ('tot_ns', float),('err_tot_ns', float), ('delay_ns', float), ('err_delay_ns', float)])
-            in_file_h5.createTable(in_file_h5.root, 'tdc_data', avg_tab, filters=self.filter_tables)
-
-        p1, p2 = plotting.plot_timewalk(h5_filename)
-        output_file(self.output_filename + '.html', title=self.run_name)
-        save(vplot(p1,p2))
-
-
-
 if __name__ == "__main__":
-
     scan = ThresholdScan()
     scan.start(**local_configuration)
     scan.tdc_table()
